d8f98e6a0609a9b5d7f11b42101d4a92160bf305
shayer23 <shayer@mantech-inc.com>
2025-03-07 17:50:53 -0500

    adding package json

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-const http = require("http");
-const url = require("url");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url, true).query;
-  console.log("Received Data:", queryObject);
-
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("ESP8266 Data Received\n");
+app.get("/", (req, res) => {
+  console.log("Received Data:", req.query);
+  res.send("ESP8266 Data Received\n");
 });
 
-const PORT = 80; // Port 80 for easy access
-server.listen(PORT, () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
